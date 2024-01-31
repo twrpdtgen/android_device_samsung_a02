474ce85a084f891bef2a80e6b48487743ec96ab5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-31 13:03:23 +0100

    a02: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: samsung Model: SM-A022M Script version: 3.0.0

---
+++ omni_a02.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a02 device
+$(call inherit-product, device/samsung/a02/device.mk)
+
+PRODUCT_DEVICE := a02
+PRODUCT_NAME := omni_a02
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A022M
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a02ub-user 11 RP1A.200720.012 A022MUBS3BVB2 release-keys"
+
+BUILD_FINGERPRINT := samsung/a02ub/a02:11/RP1A.200720.012/A022MUBS3BVB2:user/release-keys
